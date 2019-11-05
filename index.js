1f038f692fbaf00cce61bf3f54e65606621c3de9
Louis BENOIST--FOUCHER <louis.benoist-foucher@estiam.com>
2019-11-05 16:18:19 +0100

    Fix CORS error

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 // create express app
 const app = express();
+
+// CORS
+app.use(cors());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
